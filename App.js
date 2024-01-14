c6f265d5cfa84fc3f174569a6037e1212ca97695
banggeunho <panggeunho@gmail.com>
2024-01-14 23:55:45 +0900

    feat: Apply new Layout using View

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <View>
-          <Text
-              style={styles.dummyText}
-          >
-            Another piece of text!
-          </Text>
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput}/>
+                <Button title="Add Goal"/>
+            </View>
+            <View style={styles.goalsContainer}>
+                <Text>List of goals...</Text>
+            </View>
         </View>
-        <View>
-          <Text
-              style={styles.dummyText}
-          >
-            Hello World!
-          </Text>
-        </View>
-        <Button title="Tap me! "/>
-      </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'blue'
-  }
+    appContainer: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: "#cccccc"
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: "#cccccc",
+        width: "70%",
+        marginRight: 8,
+        padding: 8,
+    },
+    goalsContainer: {
+        flex: 5
+    }
+
 });
