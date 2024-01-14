3fb1607124638de663b82cb04b52f3434e8ab673
banggeunho <panggeunho@gmail.com>
2024-01-14 23:59:53 +0900

    feat: Add Handler for inputtext

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+    const [enteredGoalText, setEnteredGoalText] = useState("");
+
+    function goalInputHandler(enteredText) {
+        console.log(enteredText);
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler() {
+        console.log(enteredGoalText);
+    }
+
+
     return (
         <View style={styles.appContainer}>
             <View style={styles.inputContainer}>
-                <TextInput style={styles.textInput}/>
-                <Button title="Add Goal"/>
+                <TextInput style={styles.textInput}
+                           placeholder={"your course goal!"}
+                           onChangeText={goalInputHandler}
+                />
+                <Button title="Add Goal"
+                        onPress={addGoalHandler}/>
             </View>
             <View style={styles.goalsContainer}>
                 <Text>List of goals...</Text>
@@ -39,5 +56,4 @@ const styles = StyleSheet.create({
     goalsContainer: {
         flex: 5
     }
-
 });
