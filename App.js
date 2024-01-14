bafcb87029c6d3051a27211920adc2f3e9a05840
banggeunho <panggeunho@gmail.com>
2024-01-14 23:42:01 +0900

    feat: add new styles

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View>
+          <Text
+              style={styles.dummyText}
+          >
+            Another piece of text!
+          </Text>
+        </View>
+        <View>
+          <Text
+              style={styles.dummyText}
+          >
+            Hello World!
+          </Text>
+        </View>
+        <Button title="Tap me! "/>
+      </View>
   );
 }
 
@@ -17,4 +30,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue'
+  }
 });
