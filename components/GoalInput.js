6fa8f400a03fd36ecc25ad9a420552c4288d2718
banggeunho <panggeunho@gmail.com>
2024-01-23 23:39:13 +0900

    feat: 모달 종료버튼 생성

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -26,7 +26,7 @@ const GoalInput = (props) => {
                         <Button title="Add Goal" onPress={addGoalHandler}/>
                     </View>
                     <View style={styles.button}>
-                        <Button title="Cancel"/>
+                        <Button title="Cancel" onPress={props.onEndAddGoal}/>
                     </View>
                 </View>
             </View>
